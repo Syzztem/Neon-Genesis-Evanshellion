6edd11b21f1e6ad119fa5ed06513b6964c6a6b1a
Louis <louis.thievent@gmail.com>
2021-01-08 17:36:50 +0100

    added support for single quotes

--- methods.c
+++ methods.c
@@ -6,7 +6,7 @@
 /*   By: lothieve <lothieve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 17:06:11 by lothieve          #+#    #+#             */
-/*   Updated: 2021/01/08 14:11:31 by lothieve         ###   ########.fr       */
+/*   Updated: 2021/01/08 17:36:37 by lothieve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*spaces(char **line)
 {
-	t_token	*out;
 	size_t	len;
 
 	len = 0;
@@ -22,17 +21,31 @@ char	*spaces(char **line)
 	{
 		len++;
 		(*line)++;
+		if (**line == '\\' && *((*line) + 1))
+		{
+			(*line) += 2;
+			len += 2;
+		}
 	}
 	return (ft_strndup(*line - len, len));
 }
 
 char	*squotes(char **line)
 {
-	return (NULL);
+	char	*token;
+	size_t	len;
+
+	len = 0;
+	while (*(++*line) && **line != '\'')
+		++len;
+	token = ft_strndup(*line - len, len);
+	++(*line);
+	return (token);
 }
 
 char	*dquotes(char **line)
 {
+	(void)line;
 	return (NULL);
 }
 
