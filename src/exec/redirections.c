5b35de8bd0f9b17b77b063a0c5287e43072f6be8
smaccary <smaccary@student.42.fr>
2021-05-11 01:27:44 +0200

    better checking and error managements for redirections

--- src/exec/redirections.c
+++ src/exec/redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 13:30:00 by smaccary          #+#    #+#             */
-/*   Updated: 2021/05/09 00:01:45 by user42           ###   ########.fr       */
+/*   Updated: 2021/05/11 01:25:17 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,19 @@ void
 		dup2_check(rdr->stdin_dup, 0);
 }
 
-void
+int
+	abort_redirections(char **redirection, int *in_fd, int *out_fd)
+{
+	close_checked(*in_fd);
+	close_checked(*out_fd);
+	*in_fd = -1;
+	*out_fd = -1;
+	psyntax_error(redirection[0]);
+	g_exit_status = 2;
+	return (-1);
+}
+
+int
 	create_files(char **redirections, int *in_fd, int *out_fd)
 {
 	char	**current;
@@ -32,6 +44,8 @@ void
 	{
 		if (tab_find_last_token(current, redirects()))
 		{
+			if (current[1] == NULL || current[1][0] == 0)
+				return (abort_redirections(current, in_fd, out_fd));
 			fd = open_redir(current);
 			if (**current == '<')
 			{
@@ -46,6 +60,7 @@ void
 		}
 		current++;
 	}
+	return (0);
 }
 
 int
@@ -58,7 +73,8 @@ int
 	rdr->stdout_dup = -1;
 	if (!redirections || !*redirections)
 		return (0);
-	create_files(redirections, &rdr->in_fd, &rdr->out_fd);
+	if (create_files(redirections, &rdr->in_fd, &rdr->out_fd) < 0)
+		return (-1);
 	if (rdr->in_fd < 0 || rdr->out_fd < 0)
 		return (-1);
 	rdr->stdin_dup = dup(0);
